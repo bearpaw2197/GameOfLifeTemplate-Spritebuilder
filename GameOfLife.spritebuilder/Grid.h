070cb2fe45ed00c081ee2cf1bb9373f9ad3183b2
bearpaw2197 <bearpaw2197@outlook.com>
2015-06-03 21:45:05 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -13,7 +13,7 @@
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
 
-- (void)envoleStep;
+- (void)evolveStep;
 - (void)countNeighbors;
 - (void)updateCreatures;
 
--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -185,29 +185,24 @@ static const int GRID_COLUMNS = 10;
                                 if( currentCreature.livingNeighbors == 3)
                                     {
                                        currentCreature.isAlive = TRUE;
+                                    
+         
+                                        if(currentCreature.livingNeighbors <= 1 ||currentCreature.livingNeighbors >= 4)
+                                    
+                                        {
+                                        
+                                            currentCreature.isAlive = FALSE;
+                                    
+                                        }
+                                   
+                                    
                                     }
-                    
-        
-                                else if(currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4)
-                                {
-                                    currentCreature.isAlive = FALSE;
-                                }
                         }
-    
-   
                     }
                 }
             }
         }
     }
-
-
-
-
-
-
-
-
-
+}
 
 @end
